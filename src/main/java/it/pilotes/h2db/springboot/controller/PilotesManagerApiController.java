a5337e548947f20e435f8748a8965a1a734ee4db
Luca Antonelli <luca.antonelli74@outlook.it>
2023-03-09 20:27:38 +0100

    added firs test

--- src/main/java/it/pilotes/h2db/springboot/controller/PilotesManagerApiController.java
+++ src/main/java/it/pilotes/h2db/springboot/controller/PilotesManagerApiController.java
@@ -17,6 +17,7 @@ import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
